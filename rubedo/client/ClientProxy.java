d2798c5bf70a0bac533006aa0827122d9992e349
Katalliaan <josh.koz@gmail.com>
2014-02-24 23:27:32 -0500

    Added ClientProxy

---
+++ rubedo/client/ClientProxy.java
@@ -0,0 +1,14 @@
+package rubedo.client;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.client.MinecraftForgeClient;
+import rubedo.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                //Stub Method
+        }
+        
+}
