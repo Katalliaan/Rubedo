43c53bd7f6b6994ea4804629b9772a43b113c0f1
Katalliaan <josh.koz@gmail.com>
2014-03-14 11:35:01 -0400

    FIXED: Missing in ContentAI

--- src/main/java/rubedo/common/ContentAI.java
+++ src/main/java/rubedo/common/ContentAI.java
@@ -13,5 +13,6 @@ public class ContentAI implements IContent {
 		// Adding AI EventHandlers
 		MinecraftForge.EVENT_BUS.register(new rubedo.ai.EntityAnimalEventHandler());
 		MinecraftForge.EVENT_BUS.register(new rubedo.ai.EntityLivingEventHandler());
+		MinecraftForge.EVENT_BUS.register(new rubedo.ai.EntityMonsterEventHandler());
 	}
 }
