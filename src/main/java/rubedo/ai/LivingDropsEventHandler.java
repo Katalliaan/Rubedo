379795477626c7a598de2b2d1d1a05bf3f07de35
Katalliaan <josh.koz@gmail.com>
2014-08-21 00:39:39 -0400

    Pigmen experiment. Drops changed from gold to silver. Goal is to make them use silver in the future

---
+++ src/main/java/rubedo/ai/LivingDropsEventHandler.java
@@ -0,0 +1,23 @@
+package rubedo.ai;
+
+import rubedo.common.ContentWorld;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+
+public class LivingDropsEventHandler {
+	@ForgeSubscribe
+	public void onEntityDrop(LivingDropsEvent event) {
+		if (event.entity instanceof EntityPigZombie) {
+			for (EntityItem drop : event.drops) {
+				if (drop.getEntityItem().itemID == Item.ingotGold.itemID)
+					drop.setEntityItemStack(new ItemStack(ContentWorld.metalItems, drop.getEntityItem().stackSize, ContentWorld.metalItems.getTextureIndex("silver_ingot")));
+				if (drop.getEntityItem().itemID == Item.goldNugget.itemID)
+					drop.setEntityItemStack(new ItemStack(ContentWorld.metalItems, drop.getEntityItem().stackSize, ContentWorld.metalItems.getTextureIndex("silver_nugget")));
+			}
+		}
+	}
+}
