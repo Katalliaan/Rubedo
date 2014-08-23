3c4fd4af29f1c01b688c0150bb9722488aafda01
Katalliaan <josh.koz@gmail.com>
2014-08-23 10:21:34 -0400

    Replaced canHarvest Block with something a bit more useful, although metadata is an issue - need to find way to get metadata of block being mined

--- src/main/java/rubedo/items/tools/ToolPickaxe.java
+++ src/main/java/rubedo/items/tools/ToolPickaxe.java
@@ -1,10 +1,12 @@
 package rubedo.items.tools;
 
 import java.util.List;
+import java.util.Map;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import rubedo.common.ContentTools;
 
 public class ToolPickaxe extends ToolBase {
@@ -59,42 +61,6 @@ public class ToolPickaxe extends ToolBase {
 	}
 
 	public boolean canHarvestBlock(Block par1Block, ItemStack itemStack) {
-		this.getToolProperties(itemStack).getMiningLevel();
-		
-		
-		
-		return par1Block == Block.obsidian
-				? this.getToolProperties(itemStack).getMiningLevel() == 3
-				: (par1Block != Block.blockDiamond
-						&& par1Block != Block.oreDiamond
-						? (par1Block != Block.oreEmerald
-								&& par1Block != Block.blockEmerald
-								? (par1Block != Block.blockGold
-										&& par1Block != Block.oreGold
-										? (par1Block != Block.blockIron
-												&& par1Block != Block.oreIron
-												? (par1Block != Block.blockLapis
-														&& par1Block != Block.oreLapis
-														? (par1Block != Block.oreRedstone
-																&& par1Block != Block.oreRedstoneGlowing
-																? (par1Block.blockMaterial == Material.rock
-																		? true
-																		: (par1Block.blockMaterial == Material.iron
-																				? true
-																				: par1Block.blockMaterial == Material.anvil))
-																: this.getToolProperties(
-																		itemStack)
-																		.getMiningLevel() >= 2)
-														: this.getToolProperties(
-																itemStack)
-																.getMiningLevel() >= 1)
-												: this.getToolProperties(
-														itemStack)
-														.getMiningLevel() >= 1)
-										: this.getToolProperties(itemStack)
-												.getMiningLevel() >= 2)
-								: this.getToolProperties(itemStack)
-										.getMiningLevel() >= 2)
-						: this.getToolProperties(itemStack).getMiningLevel() >= 2);
+		return MinecraftForge.getBlockHarvestLevel(par1Block, 0, "pickaxe") <= this.getToolProperties(itemStack).getMiningLevel();
 	}
 }
