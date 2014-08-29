b35f8a09d80ed97adda06e2c564bf338c601c358
Katalliaan <josh.koz@gmail.com>
2014-08-29 18:48:15 -0400

    First pass at retreating skeletons

---
+++ src/main/java/rubedo/ai/EntityAIFallBackSelector.java
@@ -0,0 +1,22 @@
+package rubedo.ai;
+
+import net.minecraft.command.IEntitySelector;
+import net.minecraft.entity.Entity;
+
+class EntityAIFallBackSelector implements IEntitySelector
+{
+    final EntityAIFallBack entityAvoiderAI;
+
+    EntityAIFallBackSelector(EntityAIFallBack par1EntityAIAvoidEntity)
+    {
+        this.entityAvoiderAI = par1EntityAIAvoidEntity;
+    }
+
+    /**
+     * Return whether the specified entity is applicable to this filter.
+     */
+    public boolean isEntityApplicable(Entity par1Entity)
+    {
+        return par1Entity.isEntityAlive() && EntityAIFallBack.getCreature(this.entityAvoiderAI).getEntitySenses().canSee(par1Entity);
+    }
+}
