1d912f4a915cb49d70ee9b070779d65894c68fb2
Simon Meskens <meskens.simon@gmail.com>
2014-09-28 01:52:45 +0200

    Fixed reflection bugs in release builds

--- src/main/java/rubedo/ai/EntityAIGroundEater.java
+++ src/main/java/rubedo/ai/EntityAIGroundEater.java
@@ -2,11 +2,11 @@ package rubedo.ai;
 
 import java.util.List;
 
-import rubedo.util.ReflectionHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityAnimal;
+import rubedo.util.ReflectionHelper;
 
 /**
  * Enables mobs to eat breeding food from the ground
@@ -23,8 +23,8 @@ public class EntityAIGroundEater extends EntityAIBase {
 	 */
 	@Override
 	public boolean shouldExecute() {
-		return entity.isEntityAlive() && !entity.isInLove()
-				&& entity.getGrowingAge() == 0;
+		return this.entity.isEntityAlive() && !this.entity.isInLove()
+				&& this.entity.getGrowingAge() == 0;
 	}
 
 	/**
@@ -32,18 +32,18 @@ public class EntityAIGroundEater extends EntityAIBase {
 	 */
 	@Override
 	public boolean continueExecuting() {
-		if (entity.isEntityAlive() && !entity.isInLove()
-				&& entity.getGrowingAge() == 0) {
+		if (this.entity.isEntityAlive() && !this.entity.isInLove()
+				&& this.entity.getGrowingAge() == 0) {
 			@SuppressWarnings("rawtypes")
-			List nearbyItems = entity.worldObj
-					.getEntitiesWithinAABBExcludingEntity(entity,
-							entity.boundingBox.expand(1.0D, 0.0D, 1.0D));
+			List nearbyItems = this.entity.worldObj
+					.getEntitiesWithinAABBExcludingEntity(this.entity,
+							this.entity.boundingBox.expand(1.0D, 0.0D, 1.0D));
 
 			if (nearbyItems != null && nearbyItems.size() > 0) {
 				for (int itemId = 0; itemId < nearbyItems.size(); itemId++) {
 					Entity e = (Entity) nearbyItems.get(itemId);
 					if ((e instanceof EntityItem)
-							&& entity.isBreedingItem(((EntityItem) e)
+							&& this.entity.isBreedingItem(((EntityItem) e)
 									.getEntityItem())) {
 						return true;
 					}
@@ -59,22 +59,23 @@ public class EntityAIGroundEater extends EntityAIBase {
 	 */
 	@Override
 	public void startExecuting() {
-		if (entity.isEntityAlive() && !entity.isInLove()
-				&& entity.getGrowingAge() == 0) {
+		if (this.entity.isEntityAlive() && !this.entity.isInLove()
+				&& this.entity.getGrowingAge() == 0) {
 			@SuppressWarnings("rawtypes")
-			List nearbyItems = entity.worldObj
-					.getEntitiesWithinAABBExcludingEntity(entity,
-							entity.boundingBox.expand(2.0D, 0.0D, 2.0D));
+			List nearbyItems = this.entity.worldObj
+					.getEntitiesWithinAABBExcludingEntity(this.entity,
+							this.entity.boundingBox.expand(2.0D, 0.0D, 2.0D));
 			if (nearbyItems != null && nearbyItems.size() > 0) {
 				for (int itemId = 0; itemId < nearbyItems.size(); itemId++) {
 					Entity e = (Entity) nearbyItems.get(itemId);
 					if ((e instanceof EntityItem)
-							&& entity.isBreedingItem(((EntityItem) e)
+							&& this.entity.isBreedingItem(((EntityItem) e)
 									.getEntityItem())) {
 						if (((EntityItem) e).getEntityItem().stackSize >= 1) {
 							// making the love time higher, due to the fact that
 							// other AI might need to catch up
-							ReflectionHelper.setField(entity, "inlove", 2400);
+							ReflectionHelper.setField(this.entity, "inlove",
+									2400);
 
 							((EntityItem) e).getEntityItem().stackSize--;
 
@@ -84,45 +85,37 @@ public class EntityAIGroundEater extends EntityAIBase {
 
 							// sound/particle not working all the time for some
 							// reason, needs research....
-							entity.worldObj
+							this.entity.worldObj
 									.playSoundAtEntity(
-											entity,
+											this.entity,
 											"random.pop",
 											0.2F,
-											((entity.getRNG().nextFloat() - entity
+											((this.entity.getRNG().nextFloat() - this.entity
 													.getRNG().nextFloat()) * 0.7F + 1.0F) * 2.0F);
 
-							entity.worldObj
-									.playSoundEffect(
-											entity.posX,
-											entity.posY,
-											entity.posZ,
-											"random.explosion",
-											1.0F,
-											entity.worldObj.rand.nextFloat() * 0.1F + 0.9F);
+							this.entity.worldObj
+									.playSoundEffect(this.entity.posX,
+											this.entity.posY, this.entity.posZ,
+											"random.explosion", 1.0F,
+											this.entity.worldObj.rand
+													.nextFloat() * 0.1F + 0.9F);
 
-							double d0 = entity.getRNG().nextGaussian() * 0.02D;
-							double d1 = entity.getRNG().nextGaussian() * 0.02D;
-							double d2 = entity.getRNG().nextGaussian() * 0.02D;
+							double d0 = this.entity.getRNG().nextGaussian() * 0.02D;
+							double d1 = this.entity.getRNG().nextGaussian() * 0.02D;
+							double d2 = this.entity.getRNG().nextGaussian() * 0.02D;
 
-							entity.worldObj
-									.spawnParticle(
-											"heart",
-											entity.posX
-													+ (double) (entity.getRNG()
-															.nextFloat()
-															* entity.width * 2.0F)
-													- (double) entity.width,
-											entity.posY
-													+ 0.5D
-													+ (double) (entity.getRNG()
-															.nextFloat() * entity.height),
-											entity.posZ
-													+ (double) (entity.getRNG()
-															.nextFloat()
-															* entity.width * 2.0F)
-													- (double) entity.width,
-											d0, d1, d2);
+							this.entity.worldObj.spawnParticle("heart",
+									this.entity.posX
+											+ this.entity.getRNG().nextFloat()
+											* this.entity.width * 2.0F
+											- this.entity.width,
+									this.entity.posY + 0.5D
+											+ this.entity.getRNG().nextFloat()
+											* this.entity.height,
+									this.entity.posZ
+											+ this.entity.getRNG().nextFloat()
+											* this.entity.width * 2.0F
+											- this.entity.width, d0, d1, d2);
 							break;
 						}
 					}
---
+++ src/main/java/rubedo/util/MCPHelper.java
@@ -0,0 +1,58 @@
+package rubedo.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.launchwrapper.Launch;
+
+public class MCPHelper {
+	private static final Map<String, String> fieldMap = new HashMap<String, String>();
+
+	public static String getField(String name) {
+		if ((!(Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment"))
+				&& fieldMap.containsKey(name)) {
+			return fieldMap.get(name);
+		}
+		return name;
+	}
+
+	static {
+		// Items.class
+		fieldMap.put("wooden_axe", "field_151053_p");
+		fieldMap.put("wooden_hoe", "field_151017_I");
+		fieldMap.put("wooden_pickaxe", "field_151039_o");
+		fieldMap.put("wooden_shovel", "field_151038_n");
+		fieldMap.put("wooden_sword", "field_151041_m");
+
+		fieldMap.put("stone_axe", "field_151049_t");
+		fieldMap.put("stone_hoe", "field_151018_J");
+		fieldMap.put("stone_pickaxe", "field_151050_s");
+		fieldMap.put("stone_shovel", "field_151051_r");
+		fieldMap.put("stone_sword", "field_151052_q");
+
+		fieldMap.put("iron_axe", "field_151036_c");
+		fieldMap.put("iron_hoe", "field_151019_K");
+		fieldMap.put("iron_pickaxe", "field_151035_b");
+		fieldMap.put("iron_shovel", "field_151037_a");
+		fieldMap.put("iron_sword", "field_151040_l");
+
+		fieldMap.put("golden_axe", "field_151006_E");
+		fieldMap.put("golden_hoe", "field_151013_M");
+		fieldMap.put("golden_pickaxe", "field_151005_D");
+		fieldMap.put("golden_shovel", "field_151011_C");
+		fieldMap.put("golden_sword", "field_151010_B");
+
+		fieldMap.put("diamond_axe", "field_151056_x");
+		fieldMap.put("diamond_hoe", "field_151012_L");
+		fieldMap.put("diamond_pickaxe", "field_151046_w");
+		fieldMap.put("diamond_shovel", "field_151047_v");
+		fieldMap.put("diamond_sword", "field_151048_u");
+
+		// RegistryNamespaced.class
+		fieldMap.put("registryObjects", "field_82596_a");
+		fieldMap.put("underlyingIntegerMap", "field_148759_a");
+
+		// EntityAnimal.class
+		fieldMap.put("inLove", "field_70881_d");
+	}
+}
