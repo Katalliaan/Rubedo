fc7d408f18457bc2bb5975ea2705eb8769b26764
Simon Meskens <meskens.simon@gmail.com>
2014-10-17 23:01:28 +0200

    Blacksmith recipes are in

--- src/main/java/rubedo/common/ContentBlackSmith.java
+++ src/main/java/rubedo/common/ContentBlackSmith.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import rubedo.RubedoCore;
 import rubedo.blocks.BlockMagmaFurnace;
 import rubedo.client.gui.GuiHandler;
@@ -73,6 +75,32 @@ public class ContentBlackSmith extends Singleton<ContentBlackSmith> implements
 				this.magmaSmelting.addSmeltingRecipe(
 						material.getToolHead("unrefined"),
 						material.getToolHead("hot"), null, 0.0F);
+
+				// Sword heads
+				GameRegistry.addRecipe(new ShapedOreRecipe(material
+						.getToolHead("sword"), "X", "X", "Y", //
+						'X', new ItemStack(Items.clay_ball), //
+						'Y', material.getToolHead("hot")));
+				// Shovel heads
+				GameRegistry.addRecipe(new ShapedOreRecipe(material
+						.getToolHead("shovel"), "XXY", "XX ", //
+						'X', new ItemStack(Items.clay_ball), //
+						'Y', material.getToolHead("hot")));
+				// Axe heads
+				GameRegistry.addRecipe(new ShapedOreRecipe(material
+						.getToolHead("axe"), true, "XX", " X", " Y", //
+						'X', new ItemStack(Items.clay_ball), //
+						'Y', material.getToolHead("hot")));
+				// Scythe heads
+				GameRegistry.addRecipe(new ShapedOreRecipe(material
+						.getToolHead("scythe"), true, "XXX", "XY ", //
+						'X', new ItemStack(Items.clay_ball), //
+						'Y', material.getToolHead("hot")));
+				// Pick heads
+				GameRegistry.addRecipe(new ShapedOreRecipe(material
+						.getToolHead("pickaxe"), "XXX", " Y ", //
+						'X', new ItemStack(Items.clay_ball), //
+						'Y', material.getToolHead("hot")));
 			}
 		}
 	}
