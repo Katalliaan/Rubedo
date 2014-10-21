09412157488d57e6b4fe553be678496614aa50e0
Simon Meskens <meskens.simon@gmail.com>
2014-10-21 07:42:27 +0200

    Trying to fix server-side bug

--- src/main/java/rubedo/blocks/behavior/IBlockBehavior.java
+++ src/main/java/rubedo/blocks/behavior/IBlockBehavior.java
@@ -7,17 +7,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.common.util.ForgeDirection;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public interface IBlockBehavior {
-	@SideOnly(Side.CLIENT)
 	Set<String> getTextures();
 
-	@SideOnly(Side.CLIENT)
 	void setIcon(String texture, IIcon icon);
 
-	@SideOnly(Side.CLIENT)
 	IIcon getIcon(int side, int meta);
 
 	Collection<ItemStack> getSubBlocks(Item item);
