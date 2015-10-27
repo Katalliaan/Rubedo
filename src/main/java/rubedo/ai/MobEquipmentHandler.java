c9d88d97e72591d92c0d94b766f69cedb2062282
Simon Meskens <meskens.simon@gmail.com>
2015-10-27 15:20:37 +0100

    Added more mob tools

--- src/main/java/rubedo/ai/MobEquipmentHandler.java
+++ src/main/java/rubedo/ai/MobEquipmentHandler.java
@@ -17,6 +17,10 @@ import rubedo.common.materials.MaterialMultiItem.Gold;
 import rubedo.common.materials.MaterialMultiItem.Iron;
 import rubedo.common.materials.MaterialMultiItem.Silver;
 import rubedo.common.materials.MaterialMultiItem.Wood;
+import rubedo.items.tools.ToolAxe;
+import rubedo.items.tools.ToolPickaxe;
+import rubedo.items.tools.ToolScythe;
+import rubedo.items.tools.ToolShovel;
 import rubedo.items.tools.ToolSword;
 import rubedo.util.Singleton;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -27,6 +31,10 @@ public class MobEquipmentHandler {
 		ContentTools content = Singleton.getInstance(ContentTools.class);
 		ContentAI contentAI = Singleton.getInstance(ContentAI.class);
 		ToolSword sword = content.getItem(ToolSword.class);
+		ToolAxe axe = content.getItem(ToolAxe.class);
+		ToolShovel shovel = content.getItem(ToolShovel.class);
+		ToolPickaxe pick = content.getItem(ToolPickaxe.class);
+		ToolScythe scythe = content.getItem(ToolScythe.class);
 
 		Item heldItem = event.entityLiving.getHeldItem() != null ? event.entityLiving
 				.getHeldItem().getItem() : null;
@@ -49,6 +57,46 @@ public class MobEquipmentHandler {
 
 		if (heldItem == Items.iron_sword) {
 			ItemStack itemStack = sword.buildTool(
+					content.getMaterial(Iron.class),
+					content.getMaterial(Bone.class),
+					content.getMaterial(Iron.class));
+			itemStack.stackSize = 1;
+
+			event.entity.setCurrentItemOrArmor(0, itemStack);
+		}
+
+		if (heldItem == Items.iron_axe) {
+			ItemStack itemStack = axe.buildTool(
+					content.getMaterial(Iron.class),
+					content.getMaterial(Bone.class),
+					content.getMaterial(Iron.class));
+			itemStack.stackSize = 1;
+
+			event.entity.setCurrentItemOrArmor(0, itemStack);
+		}
+
+		if (heldItem == Items.iron_shovel) {
+			ItemStack itemStack = shovel.buildTool(
+					content.getMaterial(Iron.class),
+					content.getMaterial(Bone.class),
+					content.getMaterial(Iron.class));
+			itemStack.stackSize = 1;
+
+			event.entity.setCurrentItemOrArmor(0, itemStack);
+		}
+
+		if (heldItem == Items.iron_pickaxe) {
+			ItemStack itemStack = pick.buildTool(
+					content.getMaterial(Iron.class),
+					content.getMaterial(Bone.class),
+					content.getMaterial(Iron.class));
+			itemStack.stackSize = 1;
+
+			event.entity.setCurrentItemOrArmor(0, itemStack);
+		}
+
+		if (heldItem == Items.iron_hoe) {
+			ItemStack itemStack = scythe.buildTool(
 					content.getMaterial(Iron.class),
 					content.getMaterial(Bone.class),
 					content.getMaterial(Iron.class));
