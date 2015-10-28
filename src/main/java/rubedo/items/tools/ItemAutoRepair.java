8f8e53dea17a4620afe0e3e3ff0007ce5337937d
Simon Meskens <meskens.simon@gmail.com>
2015-10-28 05:42:10 +0100

    Made it so the auto repair doesn't uselessly repair items, wasting levels

--- src/main/java/rubedo/items/tools/ItemAutoRepair.java
+++ src/main/java/rubedo/items/tools/ItemAutoRepair.java
@@ -1,16 +1,15 @@
 package rubedo.items.tools;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import rubedo.RubedoCore;
-import rubedo.common.materials.MaterialMultiItem.MaterialType;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import rubedo.RubedoCore;
+import rubedo.common.materials.MaterialMultiItem.MaterialType;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemAutoRepair extends Item {
 	protected IIcon icon;
@@ -22,31 +21,35 @@ public class ItemAutoRepair extends Item {
 		this.setCreativeTab(RubedoCore.creativeTabTools);
 		this.setTextureName(RubedoCore.modid + ":autorepair");
 	}
-	
+
+	@Override
 	@SideOnly(Side.CLIENT)
-    public boolean hasEffect(ItemStack stack)
-    {
-        return true;
-    }
+	public boolean hasEffect(ItemStack stack) {
+		return true;
+	}
 
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		return "rubedo.tools.autorepair";
 	}
-	
+
 	@Override
 	public void onUpdate(ItemStack itemStack, World world,
 			Entity holdingEntity, int p_77663_4_, boolean inHand) {
 		if (!world.isRemote && holdingEntity instanceof EntityPlayer) {
 			ItemStack stack = ((EntityPlayer) holdingEntity).getHeldItem();
 
-			if (stack != null && stack.getItem() instanceof ToolBase && ((EntityPlayer) holdingEntity).experienceLevel > 0) {
+			if (stack != null && stack.getItem() instanceof ToolBase
+					&& ((EntityPlayer) holdingEntity).experienceLevel > 0) {
 				ToolProperties properties = ((ToolBase) stack.getItem())
 						.getToolProperties(stack);
 
-				if (!properties.isBroken() && properties.getMaterialType() == MaterialType.METAL_ARCANE && stack.getItemDamage() > 0) {
+				if (!properties.isBroken()
+						&& properties.getMaterialType() == MaterialType.METAL_ARCANE
+						&& stack.getItemDamage() > 5) {
 					((EntityPlayer) holdingEntity).addExperienceLevel(-1);
-					stack.setItemDamage(stack.getItemDamage() - Math.min(stack.getItemDamage(), 5));
+					stack.setItemDamage(stack.getItemDamage()
+							- Math.min(stack.getItemDamage(), 5));
 				}
 			}
 		}
